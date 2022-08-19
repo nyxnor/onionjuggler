c3807cd6f1d92790b60467097880b6fb189966fb
nyxnor <nyxnor@protonmail.com>
2022-08-19 18:39:51 +0000

    safe_edit improvements to empty and auth files - handle empty auth files or torrcs - get some confs from 'tor --dump-config' - list services that are in tor_data_dir (DataDirectory)   - inform about services that are not controller by onionjuggler tor_conf   - if not managed, but on onionjuggler tor_data_dir_services, the service can still be deleted,     but it will be recreated with a new key of course when tor restarts.   - hs services not in tor_data_dir won't appear, it is very difficult to find a way to do this if '--dump-config' list services not even verified or enabled.
    - #76
    - #66

--- usr/share/onionjuggler/defaults.sh
+++ usr/share/onionjuggler/defaults.sh
@@ -214,16 +214,20 @@ check_opt_filled(){
 
 
 ## Elegantly modify files. Test the configuration with another function.
-## Original file will be hidden with a preceding dot in its name.
+## Original file will be hidden with a preceding dot in its name and
+## suffix '-orig' if it existed already or '-new' for newly created file
 ## Temporary file will have a temporary name to be easily identified.
 ## If parsing is correct, then save file back to its original location.
-## This avoids running with an invalid
-## configuration that can make a daemon fail to reload or even start
+## This avoids running with an invalid configuration that can make a
+## daemon fail to start
+## Create temporary files as follows:
+## tor_conf=/path/to/conf
 ## $ safe_edit tmp variable
 ## $ safe_edit tmp tor_conf
-## modify the "${tor_conf_tmp}"
-## use the daemon option to verify config -f "${tor_conf_tmp}"
-## $ safe_edit save tor_conf
+## modify the file assigned by the variable "${variable_tmp}" (e.g. ${tor_conf_tmp})
+## use the daemon to verify the configuration, error out if failed
+## if everything went fine, parsed correctly, save file:
+## $ safe_edit save
 safe_edit(){
   key="${2}"
   safe_edit_keys_parsed="${safe_edit_keys_parsed} ${key}"
@@ -233,43 +237,49 @@ safe_edit(){
       file_dir="${file%/*}"
       file_name="${file##*/}"
       echo "${file_name}" | grep -qF "." && file_suffix="${file_name##*.}"
-      file_dot_tmp="${file_dir}/.${file_name}-orig"
-      file_tmp_pattern="${file_name}.XXXXX"
+      ## create an empty file if not existent
+      if test -f "${file}"; then
+        file_tmp_orig_suffix="orig"
+      else
+        file_tmp_orig_suffix="new"
+        notice "Creating empty file on ${file}"
+        touch "${file}"
+      fi
+      file_tmp_orig="${file_dir}/.${file_name}-${file_tmp_orig_suffix}"
+      file_tmp_pattern="${file_name}.XXXXX.tmp"
       test -n "${file_suffix}" && file_tmp_pattern="${file_tmp_pattern}.${file_suffix}"
       file_name_tmp="$(mktemp "${file_dir}/${file_tmp_pattern}")"
       notice "Saving a copy of ${file} to ${file_name_tmp}"
       chown "${tor_conf_user_group}" "${file_name_tmp}"
-      ## create an empty file if not existent
-      test -f "${file}" || touch "${file}"
       ## copy preserving mode
       cp -p "${file}" "${file_name_tmp}"
       ## tor won't parse a hidden file
-      notice "Moving original file ${file} to ${file_dot_tmp}"
-      mv "${file}" "${file_dot_tmp}"
+      notice "Moving original file ${file} to ${file_tmp_orig}"
+      mv "${file}" "${file_tmp_orig}"
       ## delete temp file. Also, if the hidden file still exists, means it wasn't saved,
       ## so move it back to original location.
       exit_remove_file="${exit_remove_file} ${file_name_tmp}"
-      exit_restore_file="${exit_restore_file} ${file_dot_tmp} ${file_dir}/${file_name}"
+      exit_restore_file="${exit_restore_file} ${file_tmp_orig} ${file_dir}/${file_name}"
       ## assign variable_tmp
       eval "${key}"_tmp="${file_name_tmp}"
-      eval "${key}"_dot_tmp="${file_dot_tmp}"
+      eval "${key}"_tmp_orig="${file_tmp_orig}"
     ;;
     save)
       for key in ${safe_edit_keys_parsed}; do
         eval file="$(printf '%s\n' '$'"${key}")"
         eval file_name_tmp='$'"${key}_tmp"
-        if cmp -s "${file_name_tmp}" "${file_dot_tmp}"; then
-          notice "File ${file_name_tmp} does not differ from ${file_dot_tmp}"
+        if cmp -s "${file_name_tmp}" "${file_tmp_orig}"; then
+          notice "File ${file_name_tmp} does not differ from ${file_tmp_orig}"
           notice "Not writing back tmp file to original location${nocolor}"
           rm -f "${file_name_tmp}"
-          notice "Restoring original file that was mde hidden ${file_dot_tmp} to ${file}"
-          mv "${file_dot_tmp}" "${file}"
+          notice "Restoring original file that was mde hidden ${file_tmp_orig} to ${file}"
+          mv "${file_tmp_orig}" "${file}"
         else
-          notice "File ${file_name_tmp} differs from ${file_dot_tmp}"
+          notice "File ${file_name_tmp} differs from ${file_tmp_orig}"
           notice "Moving temporary ${file_name_tmp} back to its original location ${file}"
           mv "${file_name_tmp}" "${file}"
-          notice "Removing original file that was made hidden ${file_dot_tmp}"
-          rm -f "${file_dot_tmp}"
+          notice "Removing original file that was made hidden ${file_tmp_orig}"
+          rm -f "${file_tmp_orig}"
         fi
       done
     ;;
@@ -282,6 +292,7 @@ safe_edit(){
 # shellcheck disable=SC2086
 trap_exit_all(){
   trap_exit_restore ${exit_restore_file}
+  notice "Removing temporary files"
   rm -fv ${exit_remove_file}
 }
 
@@ -291,7 +302,13 @@ trap_exit_restore(){
   while test -n "${1}" && test -n "${2}"; do
     orig_copy="${1}"
     orig_place="${2}"
-    test -f "${orig_copy}" && notice "Restoring ${orig_copy} to ${orig_place}" && mv "${orig_copy}" "${orig_place}"
+    orig_copy_suffix="${1##*-}"
+    if [ "${orig_copy_suffix}" = "new" ]; then
+      notice "Removing empty file ${orig_copy}"
+      rm -fv "${orig_copy}"
+    else
+      test -f "${orig_copy}" && notice "Restoring ${orig_copy} to ${orig_place}" && mv "${orig_copy}" "${orig_place}"
+    fi
     shift
   done
 }
@@ -303,8 +320,8 @@ verify_config_tor(){
   read_tor_files
   ! ${tor_start_command:="tor"} --verify-config --hush && error_msg "aborting: configuration is invalid"
   notice "${green}Configuration OK${nocolor}"
-  ## as configuration is ok, move modified file with temp name to correct original name
-  ## remove original hidden file
+  printf '\n'
+  ## as configuration is ok, save modified file and delete temporary ones
   safe_edit save
 }
 
@@ -317,8 +334,15 @@ read_tor_files(){
     tor_start_command="$(grep "ExecStart=" /lib/systemd/system/tor.service | sed "s/ExecStart=//")"
   fi
   ## verify tor confgiuration just to grep which files were included
-  tor_verify_config_output="$(${tor_start_command:="tor"} --verify-config)"
+  ## enforce "RunAsDaemon 0" to not run as daemon
+  : "${tor_start_command:="tor"}"
+  tor_verify_config_output="$(${tor_start_command} --RunAsDaemon 0 --verify-config)"
   tor_config_files="$(printf '%s\n' "${tor_verify_config_output}" |  grep -E " Read configuration file [^ ]*| Including configuration file [^ ]*" | sed "s/.* //" | cut -d "\"" -f2 | tr "\n" " ")"
+  tor_dump_config_output="$(${tor_start_command} --RunAsDaemon 0 --dump-config short)"
+  tor_dump_config_file="${onionjuggler_tmp_dir}/dump-config"
+  tor_dump_config_hs="${tor_dump_config_file}.hs"
+  printf '%s\n' "${tor_dump_config_output}" | tee "${tor_dump_config_file}" >/dev/null
+  grep "HiddenService" "${tor_dump_config_file}" | tee "${tor_dump_config_hs}" >/dev/null
 }
 
 
@@ -384,8 +408,8 @@ is_addr_port(){
 }
 
 
-## returns 1 if not empty
 ## returns 0 if empty
+## returns 1 if not empty
 ## no better way to do with posix utilities
 is_dir_empty(){
   dir="${1}"
@@ -400,24 +424,39 @@ is_dir_empty(){
 
 
 is_service_dir_empty(){
-  is_dir_empty "${tor_data_dir_services}" || error_msg "Onion services directory is empty. Create a service first before running this command again."
+  is_dir_empty "${tor_data_dir_services}" && error_msg "Onion services directory is empty. Create a service first before running this command again."
 }
 
 
 ## test if service exists to continue the script or output error logs.
 ## if the service exists, will save the hostname for when requested.
 test_service_exists(){
-  service="${1}"
-  onion_hostname=$(grep -F ".onion" "${tor_data_dir_services}/${service}/hostname" 2>/dev/null)
-  [ -z "${onion_hostname}" ] && error_msg "Service does not exist: ${service}"
+  service_clean="${1%*/}"
+  service_base="${service_clean##*/}"
+  service_path="${service_clean%/*}"
+  if [ "${service_path}" = "${service_base}" ]; then
+    service_path="${tor_data_dir_services}"
+  fi
+  onion_hostname=$(grep -F ".onion" "${service_path}/${service_base}/hostname" 2>/dev/null)
+  [ -z "${onion_hostname}" ] && error_msg "File '${service_path}/${service_base}/hostname' does not exist"
+
+  ## find a better function to put this...
+  test -d "${tor_data_dir_services}" || mkdir -p "${tor_data_dir_services}"
 }
 
 
 ## save the clients names that are inside the <HiddenServiceDir>/authorized_clients/ in list format (CLIENT1,CLIENT2,...)
 create_client_list(){
   service="${1}"
+  service_clean="${1%*/}" ## clean service dir from trailing slash at the end
+  service_base="${service_clean##*/}" ## get only service name
+  service_path="${service_clean%/*}" ## get service path
+  if [ "${service_path}" = "${service_base}" ]; then ## if path=name then no path was given, use default path
+    service_path="${tor_data_dir_services}"
+  fi
+
   client_name_list=""
-  for client_listed in "${tor_data_dir_services}/${service}/authorized_clients"/*; do
+  for client_listed in "${service_path}/${service_base}/authorized_clients"/*; do
     client_listed="${client_listed##*/}"
     [ "${client_listed}" = "*" ] && break
     client_listed="${client_listed%*.auth}"
@@ -479,16 +518,23 @@ escape_printf_percent() { printf "%s\n" "$(printf '%s' "${1}" | sed "s/\%/\%/g")
 ## but discard every line not starting with HiddenServiceDir
 ## https://github.com/nyxnor/onionjuggler/issues/51
 service_block(){
-  process="${1}"
-  service="${2}"
+  process="${1}" ## [print|delete]
   file="${3:-"${tor_conf_tmp}"}"
+
+  service_clean="${2%*/}" ## clean service dir from trailing slash at the end
+  service_base="${service_clean##*/}" ## get only service name
+  service_path="${service_clean%/*}" ## get service path
+  if [ "${service_path}" = "${service_base}" ]; then ## if path=name then no path was given, use default path
+    service_path="${tor_data_dir_services}"
+  fi
+
   i=0
   ## print the exact match HiddenServiceDir of the requested service that must end with the service name or with "/", also prit n lines below it
-  match="HiddenServiceDir ${tor_data_dir_services}/${service}"
+  match="HiddenServiceDir ${service_path}/${service_base}"
   hs_found=""
   hs_lines_delete=""
   while IFS="$(printf '\n')" read -r line; do
-    [ -z "${hs_found}" ] && printf '%s\n' "${line}" | grep -q -E "^${match}$|^${match}/$" && hs_found="1"
+    [ -z "${hs_found}" ] && printf '%s\n' "${line}" | grep -e "${match}$" -e "${match}/$" | grep -q -v "[[:space:]]*\#" && hs_found="1"
     if [ "${hs_found}" = "1" ]; then
       i=$((i+1))
       case "${line}" in
@@ -526,15 +572,22 @@ service_block(){
 ## generate key pairs for client authorization
 gen_auth_key_pair(){
   ## Generate pem and derive pub and priv keys
-  "${openssl_cmd}" genpkey -algorithm x25519 -out /tmp/k1.prv.pem
-  grep -v " PRIVATE KEY" /tmp/k1.prv.pem | base64pem -d | tail -c 32 | base32 | sed "s/=//g" > /tmp/k1.prv.key
-  "${openssl_cmd}" pkey -in /tmp/k1.prv.pem -pubout | grep -v " PUBLIC KEY" | base64pem -d | tail -c 32 | base32 | sed "s/=//g" > /tmp/k1.pub.key
+  "${openssl_cmd}" genpkey -algorithm x25519 -out "${onionjuggler_tmp_dir}"/k1.prv.pem
+  grep -v " PRIVATE KEY" "${onionjuggler_tmp_dir}"/k1.prv.pem | base64pem -d | tail -c 32 | base32 | sed "s/=//g" > "${onionjuggler_tmp_dir}"/k1.prv.key
+  "${openssl_cmd}" pkey -in "${onionjuggler_tmp_dir}"/k1.prv.pem -pubout | grep -v " PUBLIC KEY" | base64pem -d | tail -c 32 | base32 | sed "s/=//g" > "${onionjuggler_tmp_dir}"/k1.pub.key
   ## save variables
-  client_pub_key=$(cat /tmp/k1.pub.key)
-  client_priv_key=$(cat /tmp/k1.prv.key)
+  client_pub_key=$(cat "${onionjuggler_tmp_dir}"/k1.pub.key)
+  client_priv_key=$(cat "${onionjuggler_tmp_dir}"/k1.prv.key)
   client_priv_key_config="${onion_hostname%.onion}:descriptor:x25519:${client_priv_key}"
   client_pub_key_config="descriptor:x25519:${client_pub_key}"
   ## Delete pem and keys
-  rm -f /tmp/k1.pub.key /tmp/k1.prv.key /tmp/k1.prv.pem
+  rm -f "${onionjuggler_tmp_dir}"/k1.pub.key "${onionjuggler_tmp_dir}"/k1.prv.key "${onionjuggler_tmp_dir}"/k1.prv.pem
 }
 
+
+############################
+######### ACTIONS ##########
+: "${TMPDIR:="/tmp"}"
+onionjuggler_tmp_dir="${TMPDIR%*/}/onionjuggler"
+mkdir -p "${onionjuggler_tmp_dir}"
+chmod 700 "${onionjuggler_tmp_dir}"
